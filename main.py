32574f10024dcf6fe03efc98ee5a103269c7e46e
atisam4 <136904223+atisam4@users.noreply.github.com>
2024-10-13 20:56:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def approval_request():
 @app.route('/check-permission', methods=['POST'])
 def check_permission():
     unique_key = request.form['unique_key']
-    response = requests.get("https://pastebin.com/raw/E3FLmRKx")
+    response = requests.get("https://pastebin.com/raw/VHHXxw0M")
     approved_tokens = [token.strip() for token in response.text.splitlines() if token.strip()]
     if unique_key in approved_tokens:
         print("Permission granted. You can proceed with the script.")
